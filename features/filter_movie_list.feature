d44531f972721507fbba36f31b2756a9365db6f0
saasbook <saasbook@saasbook.(none)>
2013-02-11 10:34:49 -0800

    checkboxes fixed

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,10 +23,16 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  When I check the following ratings: PG, R
   # enter step(s) to uncheck all other checkboxes
+  When I uncheck the following ratings: G, PG-13, NC-17
+  # When I uncheck ratings_R
   # enter step to "submit" the search form on the homepage
+  When I press "Refresh"
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see movies: "Raiders of the Lost Ark", "The Terminator", "When Harry Met Sally", "Amelie", "The Incredibles"
   # enter step(s) to ensure that other movies are not visible
+  Then I should not see movies: "Aladdin", "The Help", "Chocolat", "2001: A Space Odyssey", "Chicken Run"
 
 Scenario: no ratings selected
   # see assignment
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,19 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+#   create_table :movies do |t|
+#     t.string :title
+#     t.string :rating
+#     t.text :description
+#     t.datetime :release_date
+#   end
+  Movie.delete_all
   movies_table.hashes.each do |movie|
+    Movie.create(:title => movie[:title], :release_date => movie[:release_date], :rating => movie[:rating])
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,15 +22,42 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
+  assert(page.body.index(e1) < page.body.index(e2))
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+When /I (un)?check the following ratings: (.*)/ do |isUncheck, rating_list|
+  print isUncheck
+  ratings = rating_list.split(",")
+  ratings.each do |r|
+    r.strip!
+    id = "ratings_" + r
+    if isUncheck == 'un' then
+      uncheck(id)
+    else
+      check(id)
+    end
+  end
+end
+
+Then /I should not see movies: (.*)/ do |movieList|
+  movies = movieList.split(",")
+  movies.each do |m|
+    title = (m.strip)[1..-2]
+    i = page.body.index(title)
+    assert(i == nil, "Movie " + title + " found!\nIndex: " + i.to_s + "\nPage:\n" + page.body)
+  end
+end
+
+Then /I should see movies: (.*)/ do |movieList|
+  movies = movieList.split(",")
+  movies.each do |m|
+    title = (m.strip)[1..-2]
+    i = page.body.index(title)
+    assert(i != nil, "Movie " + title + " NOT found!\nIndex: " + i.to_s + "\nPage:\n" + page.body)
+  end
 end
